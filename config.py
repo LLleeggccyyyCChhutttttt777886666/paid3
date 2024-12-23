80787887a790e841fe5a83c78935acf0c2efe709
Legacychutt <yoginum2@outlook.com>
2024-12-24 02:58:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7511149271:AAF_TlnkiYooVBP9qaa3fCfFaZGi0rjlJao")
+BOT_TOKEN=("7538166069:AAG5YeTWBvW_n8aO-51R5zNRolQ6SPPjViw")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@God_x_Sikandar")
